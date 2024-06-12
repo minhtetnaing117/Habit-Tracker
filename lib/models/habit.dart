2aee0aa9def89630bdd0cffa7913a984c02b2741
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-06-12 11:45:18 +0630

    initial g.dart commit

--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -3,6 +3,7 @@ import 'package:isar/isar.dart';
 // run cmd to generate file: dart run build_runner build
 part 'habit.g.dart';
 
+@Collection()
 class Habit{
   // habit id
   Id id = Isar.autoIncrement;
